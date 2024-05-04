f763eb6b8131635609e8285b358e80c240319720
unknown <abhayagnihotri1585@gmail.com>
2024-05-04 12:48:27 +0530

    added-the-SSR-ui-of-project

--- url-shortner/index.js
+++ url-shortner/index.js
@@ -14,7 +14,9 @@ connectMongoDB('mongodb://127.0.0.1:27017/url-shortner').then(()=>console.log("M
 app.use(express.json()); 
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'stylesheets')));
 app.use(checkForAuthentication);
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views')); 
