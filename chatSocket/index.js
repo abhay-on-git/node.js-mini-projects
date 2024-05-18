6a82d6f9f8799c611007ae90945e65c4af78f493
unknown <abhayagnihotri1585@gmail.com>
2024-05-18 12:51:53 +0530

    chatSoket-added-sqlite

--- chatSocket/index.js
+++ chatSocket/index.js
@@ -1,22 +1,72 @@
-const express = require("express");
-const { createServer } = require("node:http");
-const { join } = require("node:path");
-const { Server } = require("socket.io");
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
+async function main() {
+  // open the database file
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
   });
 
-server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
+  // create our 'messages' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+  // [...]
+
+io.on('connection', async (socket) => {
+  socket.on('chat message', async (msg) => {
+    let result;
+    try {
+      result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    io.emit('chat message', msg, result.lastID);
+  });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each('SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      )
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
+
+}
+
+
+
+
+main();
